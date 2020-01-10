6be1ac265eeb6a1e0690528914da79d437f91865
A. Noval Adip <a.novaladip@yahoo.com>
2020-01-10 21:14:54 +0700

    [entity] add JwtPayloadFromRequest()

--- pkg/entity/jwtpayload.go
+++ pkg/entity/jwtpayload.go
@@ -1,6 +1,13 @@
 package entity
 
+import "github.com/gin-gonic/gin"
+
 type JwtPayload struct {
 	Id    string
 	Email string
+}
+
+func JwtPayloadFromRequest(c *gin.Context) JwtPayload {
+	user, _ := c.MustGet("JwtPayload").(JwtPayload)
+	return user
 }
