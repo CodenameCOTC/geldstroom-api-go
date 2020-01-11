58aaf83acc42859f179d4eafeda66f87ee3d055e
A. Noval Adip <a.novaladip@yahoo.com>
2020-01-11 22:31:09 +0700

    change url email verification

--- pkg/email/email.go
+++ pkg/email/email.go
@@ -9,7 +9,7 @@ import (
 )
 
 func SendEmailVerification(receiver, token string) error {
-	url := fmt.Sprintf("https://cotcapp.my.id/user/verify/email/%v", token)
+	url := fmt.Sprintf("https://geldstroom.cotcapp.my.id/user/verify/email/%v", token)
 	htmlContent := fmt.Sprintf(`<strong>To verify your email click <a href="%v">here</a></strong>`, url)
 	plainTextContent := fmt.Sprintf("To Verify your email click this link %v", url)
 	key := config.ConfigKey.SENDGRID_KEY
